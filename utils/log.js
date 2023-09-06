798a0019ae3e12b3ac3fa4e9f8c5796756002749
jklee <jklee@jkleeui-MacBookAir.local>
2023-09-06 21:44:28 +0900

    FEAT: 로그 강화

--- utils/log.js
+++ utils/log.js
@@ -1,19 +1,19 @@
-import { dateFormat } from './date.js';
+import {dateFormat} from './date.js';
 
-export const dConsole = (...args) => {   
-  console.log(`[${dateFormat(new Date(), 'YYYY-MM-DD hh:mm:ss')}]`);
-  console.log(`[Log]`, ...args);
-  console.log('================================================')
-}
+export const dConsole = (...args) => {
+	console.log(`[${dateFormat(new Date(), 'YYYY-MM-DD hh:mm:ss')}]`);
+	console.log(`[Log]`, ...args);
+	console.log('\n================================================\n');
+};
 
 export const dConsoleWarn = (...args) => {
-  console.log(`[${dateFormat(new Date(), 'YYYY-MM-DD hh:mm:ss')}]`);
-  console.warn('[Warn]', ...args);
-  console.log('================================================')
-}
+	console.log(`[${dateFormat(new Date(), 'YYYY-MM-DD hh:mm:ss')}]`);
+	console.warn('[Warn]', ...args);
+	console.log('\n================================================\n');
+};
 
 export const dConsoleError = (...args) => {
-  console.log(`[${dateFormat(new Date(), 'YYYY-MM-DD hh:mm:ss')}]`);
-  console.error('[Error] ', ...args);
-  console.log('================================================')
-}
+	console.log(`[${dateFormat(new Date(), 'YYYY-MM-DD hh:mm:ss')}]`);
+	console.error('[Error] ', ...args);
+	console.log('\n================================================\n');
+};
