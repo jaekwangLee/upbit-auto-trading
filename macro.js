8ac7679258972a361e1c9a9a3ae0e9d0d39a4d50
jklee <jklee@jkleeui-MacBookAir.local>
2024-12-31 16:38:11 +0900

    FEAT: 하락 아닐때도 판매하는 이슈 수정

--- macro.js
+++ macro.js
@@ -1,28 +1,26 @@
 /** @format */
 
-import {
-  TRADING_SYSTEM_MAX_RECOVERY,
-  TRADING_SYSTEM_RECOVERY_PERIOD,
-} from "./constant/trading.js";
-import Trader from "./lib/Trade/index.js";
+import {TRADING_SYSTEM_MAX_RECOVERY, TRADING_SYSTEM_RECOVERY_PERIOD} from './constant/trading.js';
+
+import Trader from './lib/Trade/index.js';
 
 let currRecoveryCount = 0;
 
 const runAutoTradingSystem = async () => {
-  const trader = new Trader();
+	const trader = new Trader();
 
-  try {
-    trader.build().runTickerDataCollector().runTradeWorker();
-  } catch (error) {
-    console.error(`[ERROR] trading system is gone, error: ${error}`);
+	try {
+		trader.build().runTickerDataCollector().runTradeWorker();
+	} catch (error) {
+		console.error(`[ERROR] trading system is gone, error: ${error}`);
 
-    if (currRecoveryCount < TRADING_SYSTEM_MAX_RECOVERY) {
-      currRecoveryCount++;
-      setTimeout(runAutoTradingSystem, TRADING_SYSTEM_RECOVERY_PERIOD);
-    } else {
-      trader.destroyWorkers();
-    }
-  }
+		if (currRecoveryCount < TRADING_SYSTEM_MAX_RECOVERY) {
+			currRecoveryCount++;
+			setTimeout(runAutoTradingSystem, TRADING_SYSTEM_RECOVERY_PERIOD);
+		} else {
+			trader.destroyWorkers();
+		}
+	}
 };
 
-export { runAutoTradingSystem };
+export {runAutoTradingSystem};
