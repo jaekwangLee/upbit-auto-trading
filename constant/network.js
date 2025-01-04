5795429e24651656443b008103666b8ac1a6b1be
jklee <jklee@jkleeui-MacBookAir.local>
2025-01-04 22:05:12 +0900

    FEAT: 에러핸들링 및 로깅강화 & 종목선정 잘못된 우선순위로 하던 이슈 수정

--- constant/network.js
+++ constant/network.js
@@ -1,18 +1,24 @@
 const UPBIT_NETWORK_ERROR = Object.freeze({
-  BAD_REQUEST: 400,
-  UNAUTHORIZED: 401,
+	BAD_REQUEST: 400,
+	UNAUTHORIZED: 401,
 });
 
 const REST_API_METHOD = Object.freeze({
-  POST: "post",
-  GET: "get",
-  DELETE: "delete",
+	POST: 'post',
+	GET: 'get',
+	DELETE: 'delete',
 });
 
 const CONTENT_TYPE = Object.freeze({
-  APPLICATION_JSON: "application/json",
-  MULTIPART_FILE: "multipart/form-data;",
-  X_WWW_FORM_ENCODE: "Application/x-www-form-urlencode ",
+	APPLICATION_JSON: 'application/json',
+	MULTIPART_FILE: 'multipart/form-data;',
+	X_WWW_FORM_ENCODE: 'Application/x-www-form-urlencode ',
 });
 
-export { UPBIT_NETWORK_ERROR, REST_API_METHOD, CONTENT_TYPE };
+const PROMISE_STATE = Object.freeze({
+	REJECTED: 'REJECTED',
+	FULFILLED: 'FULFILLED',
+	PENDING: 'PENDING',
+});
+
+export {UPBIT_NETWORK_ERROR, REST_API_METHOD, CONTENT_TYPE, PROMISE_STATE};
