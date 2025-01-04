627ac4de9e4154156411c1d80a5ae9ecfbd607bf
jklee <jklee@jkleeui-MacBookAir.local>
2025-01-04 23:05:47 +0900

    FEAT: 로그 기능 강화, web 조회 할수있도록 밑작업 완료

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
 /** @format */
 
-import express from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-import { runAutoTradingSystem } from "./macro.js";
-import Database from "./db.js";
+import {runAutoTradingSystem} from './macro.js';
+import Database from './db.js';
+import {Logger} from './utils/log.js';
 
 class App {
-  constructor() {
-    this.app = express();
-  }
+	constructor() {
+		this.app = express();
+	}
 
-  #appServerListener() {
-    console.log(
-      `server is listening on port ${process.env.PORT}, for api. No releated trading macro`
-    );
-  }
+	#appServerListener() {
+		console.log(`server is listening on port ${process.env.PORT}, for api. No releated trading macro`);
+	}
 
-  run() {
-    this.app.listen(process.env.PORT, this.#appServerListener);
+	run() {
+		this.app.listen(process.env.PORT, this.#appServerListener);
 
-    new Database(process.env.DATABASE);
-  }
+		new Database(process.env.DATABASE);
+
+		this.app.get('/log', (req, res) => {
+			res.json(Logger.get());
+		});
+	}
 }
 
 const app = new App();
--- utils/log.js
+++ utils/log.js
@@ -1,19 +1,63 @@
 import {dateFormat} from './date.js';
 
+export class Logger {
+	static MAX_LOGS = 100;
+
+	static logs = [];
+
+	static TYPE = {
+		DEFAULT: 'DEFAULT',
+		WARN: 'WARN',
+		ERROR: 'ERROR',
+	};
+
+	static add(type, ...message) {
+		if (this.logs.length > this.MAX_LOGS) {
+			this.logs.shift();
+		}
+
+		const log = {
+			TIME: this.timestamp(),
+			MESSAGE: message,
+		};
+
+		if (type === Logger.TYPE.WARN) {
+			console.warn(log);
+		} else if (type === Logger.TYPE.ERROR) {
+			console.error(log);
+		} else {
+			console.log(log);
+		}
+
+		this.logs.push({
+			TIME: this.timestamp(),
+			MESSAGE: message.join('\n'),
+		});
+	}
+
+	static get() {
+		const logs = [...this.logs].reverse();
+		console.log(this.logs.length);
+		return logs;
+	}
+
+	static clear() {
+		this.logs = [];
+	}
+
+	static timestamp(format = 'YYYY-MM-DD hh:mm:ss') {
+		return dateFormat(new Date(), format);
+	}
+}
+
 export const dConsole = (...args) => {
-	console.log(`[${dateFormat(new Date(), 'YYYY-MM-DD hh:mm:ss')}]`);
-	console.log(`[Log]`, ...args);
-	console.log('\n================================================\n');
+	Logger.add(Logger.TYPE.DEFAULT, ...args);
 };
 
 export const dConsoleWarn = (...args) => {
-	console.log(`[${dateFormat(new Date(), 'YYYY-MM-DD hh:mm:ss')}]`);
-	console.warn('[Warn]', ...args);
-	console.log('\n================================================\n');
+	Logger.add(Logger.TYPE.WARN, ...args);
 };
 
 export const dConsoleError = (...args) => {
-	console.log(`[${dateFormat(new Date(), 'YYYY-MM-DD hh:mm:ss')}]`);
-	console.error('[Error] ', ...args);
-	console.log('\n================================================\n');
+	Logger.add(Logger.TYPE.ERROR, ...args);
 };
